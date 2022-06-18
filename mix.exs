cf8a4beda75d8c5dea0e4cca4b20ec6b8c633d8d
Daniel <daniel_sousa.p@outlook.com>
2022-06-18 00:23:07 -0300

    feat: add validations and pbkdf2 lib

--- mix.exs
+++ mix.exs
@@ -45,7 +45,8 @@ defmodule Rockelivery.MixProject do
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      {:credo, "~> 1.6", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
+      {:pbkdf2_elixir, "~> 2.0"}
     ]
   end
 
