cada1fb4cd841440c9ad721f50fa5eeee2bb4958
Daniel <daniel_sousa.p@outlook.com>
2022-06-26 00:26:26 -0300

    test: create.json view

---
+++ test/rockelivery_web/views/users_view_test.exs
@@ -0,0 +1,19 @@
+defmodule RockeliveryWeb.UsersViewTest do
+  use RockeliveryWeb.ConnCase, async: true
+
+  import Phoenix.View
+  import Rockelivery.Factory
+
+  alias RockeliveryWeb.UsersView
+
+  test "renders create.json" do
+    user = build(:user)
+
+    response = render(UsersView, "create.json", user: user)
+
+    assert %{
+             message: "User created!",
+             user: %{id: "95b9d1d4-b59a-4a78-b784-d74fad887e26", name: "Daniel"}
+           } = response
+  end
+end
