d8d75d17bce2429b385fe438b66102a21e372649
Daniel <daniel_sousa.p@outlook.com>
2022-06-25 23:29:11 -0300

    test: create user

---
+++ test/rockelivery/users/create_test.exs
@@ -0,0 +1,33 @@
+defmodule Rockelivery.Users.CreateTest do
+  use Rockelivery.DataCase, async: true
+
+  import Rockelivery.Factory
+
+  alias Rockelivery.{Error, User}
+  alias Rockelivery.Users.Create
+
+  describe "call/1" do
+    test "it should returns the user when all params are valid" do
+      params = build(:user_params)
+
+      response = Create.call(params)
+
+      assert {:ok, %User{id: _id, age: 21, email: "daniel@hotmail.com"}} = response
+    end
+
+    test "it should returns an error, when there are invalid params" do
+      params = build(:user_params, %{age: 15, password: "123"})
+
+      response = Create.call(params)
+
+      {:error, %Error{result: changeset}} = response
+
+      expected_response = %{
+        age: ["must be greater than or equal to 18"],
+        password: ["should be at least 6 character(s)"]
+      }
+
+      assert errors_on(changeset) == expected_response
+    end
+  end
+end
