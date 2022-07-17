a640ffe034488bc1607d3dff0a6c40a4b3e3eef6
Daniel <daniel_sousa.p@outlook.com>
2022-07-17 12:00:52 -0300

    feat: order create repo

---
+++ lib/rockelivery/orders/create.ex
@@ -0,0 +1,17 @@
+defmodule Rockelivery.Orders.Create do
+  import Ecto.Query
+
+  alias Rockelivery.{Error, Item, Order, Repo}
+
+  def call(params) do
+    fetch_items(params)
+  end
+
+  defp fetch_items(%{"items" => items_params}) do
+    item_ids = Enum.map(items_params, fn item -> item["id"] end)
+
+    query = from item in Item, where: item.id in ^item_ids
+
+    Repo.all(query)
+  end
+end
