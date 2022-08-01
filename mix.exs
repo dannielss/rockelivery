5e3873a8bd125903839e31b26325f8f7d943574b
Daniel <daniel_sousa.p@outlook.com>
2022-07-31 22:18:45 -0300

    feat: http call to useviacep api

--- mix.exs
+++ mix.exs
@@ -55,7 +55,9 @@ defmodule Rockelivery.MixProject do
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
       {:pbkdf2_elixir, "~> 2.0"},
       {:excoveralls, "~> 0.10", only: :test},
-      {:ex_machina, "~> 2.7.0"}
+      {:ex_machina, "~> 2.7.0"},
+      {:tesla, "~> 1.4"},
+      {:hackney, "~> 1.17"}
     ]
   end
 
